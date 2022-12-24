65dca11330f655ead21f08983e7da1626aa6b53e
Md.Ashraful Hossain <ashrafulhossainwork@gmail.com>
2022-12-25 01:18:35 +0600

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+
+        document.write("Name :Md.Ashraful Hossain <br>");
+        document.write("Email : ashrafulhossainwork@gmail.com <br>");
+        document.write("IGN : El Professor #2000");
