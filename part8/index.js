330a299f5e597d3929af5ef7205c4256de99a171
Md.Ashraful Hossain <ashrafulhossainwork@gmail.com>
2022-12-29 01:10:13 +0600

    init

---
+++ part8/index.js
@@ -0,0 +1,18 @@
+// var num = 20.5;
+
+// num = toString(num)
+// num = parseInt(num)
+// num = parseFloat(num)
+// console.log(typeof(num));
+
+// var number = 20.5678;
+// console.log(number.toFixed(1)) this is used to show number after  " . "
+// console.log(number.toPrecision(3)) this is used to show number total numbers
+
+console.log(typeof(Number("123")))
+console.log(typeof(Number("123")))
+console.log(Number(true))
+console.log(Number(false))
+
+
+
