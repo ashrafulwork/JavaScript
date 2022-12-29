6b2fc57f6380d50d6e617e1bf88d082ffea228f2
Md.Ashraful Hossain <ashrafulhossainwork@gmail.com>
2022-12-29 20:01:28 +0600

    init

---
+++ part11/index.js
@@ -0,0 +1,3 @@
+//relational operator -> >, >=, <, <=, ==, ===, !=, !==
+
+//logical operator -> && , || , !
---
+++ part14/index.js
@@ -0,0 +1,91 @@
+//First Problem
+
+// var marks = prompt("Enter your mark : ");
+// if(marks>=80 && marks<=100){
+
+//     document.write("A+")
+// }
+// else if (marks>=75 && marks<=79){
+
+//     document.write("A")
+// }
+// else if (marks>=70 && marks<=74){
+
+//     document.write("A-")
+// }
+
+// else if (marks>=65 && marks<=69){
+
+//     document.write("B+")
+// }
+
+// else if (marks>=60 && marks<=64){
+
+//     document.write("B")
+// }
+
+// else if (marks>=55 && marks<=59){
+
+//     document.write("B-")
+// }
+
+// else if (marks>=50 && marks<=54){
+
+//     document.write("C+")
+// }
+
+
+// else if (marks>=45 && marks<=49){
+
+//     document.write("C")
+// }
+
+
+// else if (marks>=40 && marks<=44){
+
+//     document.write("D")
+// }
+
+
+// else if (marks<=39){
+
+//     document.write("Fail")
+// }
+
+
+// else {
+
+//     document.write("Incorrect Input ")
+// }
+
+
+
+// //Second problem
+
+// var num1 = prompt("Enter your first number : ");
+// var num2 = prompt("Enter your second number : ");
+// var num3 = prompt("Enter your third number : ");
+
+// if (num1>num2 && num1>num3 ){
+//     document.write("First Number " +" " + num1 + " " +"is bigger ! ");
+// }
+
+// else if (num2>num1 && num2>num3 ){
+//     document.write("First Number " + " " +num2 + " " +"is bigger ! ");
+// }
+
+// else if(num3>num1 && num3>num2 ){
+//     document.write("Third Number" +" " +num3 + " " +"is bigger ! ");
+// }
+// else 
+// document.write("Incorrect Input")
+
+// Thrid Problem
+
+var ask = prompt("Enter the Character : ");
+
+if(ask == "a"|| ask == "b" || ask == "c" || ask == "d" || ask == "e"  ){
+    document.write("Vowel")
+}
+else
+document.write("Constant");
