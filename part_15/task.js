2a8433220ba08f923f578d12fbd25147b8249c93
Md.Ashraful Hossain <ashrafulhossainwork@gmail.com>
2023-01-01 16:18:45 +0600

    init

---
+++ part_15/task.js
@@ -0,0 +1,42 @@
+
+var character = prompt("Enter any character : ");
+character = character.toLowerCase();
+switch(character){
+
+    case "a":
+        document.write("Vowel");
+        break;
+        case "e":
+            document.write("Vowel");
+            break;
+            case "i":
+                document.write("Vowel");
+                break;
+                case "o":
+                    document.write("Vowel");
+                    break;
+                    case "u":
+                        document.write("Vowel");
+                        break;
+                        
+                                            default:
+                                                document.write("Consonant");
+            
+            
+
+}
+
+//or 
+// var character = prompt("Enter any character : ");
+// character = character.toLowerCase();
+// switch(character){
+
+
+// case "a":
+//     case "e":
+//         case "i":
+//             case "o":
+//                 case "u":
+//                     document.write("Vowel");
+//                     break;
+// }
